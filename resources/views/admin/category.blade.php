09b54d747ac113504a78499c981379ebc5540dce
salimsheikh <salimsheikh4u2000@gmail.com>
2024-11-26 21:05:29 +0530

    category table

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -31,15 +31,16 @@
                     <div class="row">
                         <!-- Basic Form-->
                         <div class="col-lg-6">
-                            <div class="block">                                
+                            <div class="block">
                                 <div class="block-body">
                                     <form action="{{ url('admin/add_category') }}" method="post">
                                         @csrf
                                         <div class="form-group">
                                             <label for="category_name" class="form-control-label">Category Name</label>
-                                            <input type="text" name="category_name" placeholder="Category Name" class="form-control">
+                                            <input type="text" name="category_name" placeholder="Category Name"
+                                                class="form-control">
                                         </div>
-                                        
+
                                         <div class="form-group">
                                             <input type="submit" value="Add Category" class="btn btn-primary">
                                         </div>
@@ -47,6 +48,53 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-lg-6">
+                            <div class="block">
+                              <div class="title"><strong>Categories</strong></div>
+                              <div class="table-responsive"> 
+                                <table class="table table-striped table-sm">
+                                  <thead>
+                                    <tr>
+                                      <th>#</th>
+                                      <th>Category Name</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <th scope="row">1</th>
+                                      <td>Mark</td>
+                                      <td>Otto</td>
+                                      <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row">2</th>
+                                      <td>Jacob</td>
+                                      <td>Thornton</td>
+                                      <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row">3</th>
+                                      <td>Larry</td>
+                                      <td>the Bird</td>
+                                      <td>@twitter      </td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row">4</th>
+                                      <td>Mark</td>
+                                      <td>Otto</td>
+                                      <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                      <th scope="row">5</th>
+                                      <td>Jacob</td>
+                                      <td>Thornton</td>
+                                      <td>@fat</td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                          </div>
                     </div>
                 </div>
             </section>
@@ -56,7 +104,11 @@
     <script src="{{ asset('/admin-assets/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('/admin-assets/vendor/popper.js/umd/popper.min.js') }}"></script>
     <script src="{{ asset('/admin-assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-  
+    <script src="{{ asset('/admin-assets/vendor/jquery.cookie/jquery.cookie.js') }}"></script>
+    <script src="{{ asset('/admin-assets/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('/admin-assets/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('/admin-assets/js/charts-home.js') }}"></script>
+    <script src="{{ asset('/admin-assets/js/front.js') }}"></script>
 </body>
 
 </html>
